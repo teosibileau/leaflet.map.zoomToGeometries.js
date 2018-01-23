116da3d9a7815ee45591a2905f3d7dc5b2604cb3
Teofilo Sibileau <teo.sibileau@gmail.com>
2018-01-22 23:55:50 -0300

    Solves Compatibility Issues with Leaflet 1.3
    + Overriding L.Map no longer works for new Leaflets
    + Provides L.GeoJSONBoundedMap class with zoomToGeometries method
    + Mantains L.Map overriden for compatibility with older codebases

--- examples/app.js
+++ examples/app.js
@@ -1,10 +1,10 @@
 $.getJSON('../data/usa_states.json', function(data){
   var div = $("#map");
-  var map = new L.map(div.get(0));
+  var map = new L.GeoJSONBoundedMap(div.get(0));
   var geojson = new L.GeoJSON(data);
-  var mapUrl = "//otile{s}-s.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png";
-  var osmAttribution = 'Map data &copy; 2011 OpenStreetMap contributors, Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="//developer.mapquest.com/content/osm/mq_logo.png">';
-  var bg = new L.TileLayer(mapUrl, {maxZoom: 18, attribution: osmAttribution, subdomains: '1234'});
+  var mapUrl = "//cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png";
+  var osmAttribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>';
+  var bg = new L.TileLayer(mapUrl, {maxZoom: 18, attribution: osmAttribution, subdomains: 'abcd'});
   map.addLayer(bg);
   geojson.addTo(map);
   map.zoomToGeometries(geojson);
